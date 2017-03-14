34505211d0d05cbe13a6fe4ba2bedb8678ace890
Sullivan LACHETEAU <sullivan.lacheteau@seloger.com>
2017-03-14 23:32:56 +0100

    WorkoutPage layout + Binding

--- FitTrack/Pages/RoutinePage.xaml.cs
+++ FitTrack/Pages/RoutinePage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace FitTrack
@@ -14,9 +14,64 @@ namespace FitTrack
 			InitializeComponent();
 		}
 
-		private void OnWorkoutClicked(object sender, EventArgs e)
+		private async void OnWorkoutClicked(object sender, EventArgs e)
 		{
-			
+			var workout = new Workout
+			{
+				Name = "Legs",
+				Exercises = new ObservableCollection<Exercise>
+				{
+					new Exercise
+					{
+						Name = "Leg press",
+						Sets = "1 + 4",
+						Reps = "20 + 10 to 6",
+						Weights = "60 - 80 - 100 - 120 - 140"
+					},
+					new Exercise
+					{
+						Name = "Leg extensions",
+						Sets = "4",
+						Reps = "12 - 10 - 9 - 8",
+						Weights = "55 - 59 - 64 - 68"
+					},
+					new Exercise
+					{
+						Name = "Leg curls",
+						Sets = "4",
+						Reps = "12 - 10 - 9 - 8",
+						Weights = "50 - 55 - 59 - 64"
+					},
+					new Exercise
+					{
+						Name = "Calves raises",
+						Sets = "4",
+						Reps = "15 - 12 - 12 - 10",
+						Weights = "30 - 40 - 50 - 60"
+					}
+				}
+			};
+
+			await Navigation.PushAsync(new WorkoutPage(workout));
 		}
 	}
+
+	public class Exercise
+	{
+		public string Name { get; set; }
+
+		public string Sets { get; set; }
+
+		public string Reps { get; set; }
+
+		public string Weights { get; set; }
+	}
+
+	public class Workout
+	{
+		public string Name { get; set; }
+
+		public ObservableCollection<Exercise> Exercises { get; set; }
+	}
+
 }
---
+++ FitTrack/Pages/WorkoutPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace FitTrack
+{
+	public partial class WorkoutPage : ContentPage
+	{
+		private Workout Workout;
+
+		public ObservableCollection<Exercise> Exercises;
+
+		public WorkoutPage(Workout workout)
+		{
+			Title = workout.Name;
+			Workout = workout;
+			Exercises = new ObservableCollection<Exercise>(workout.Exercises);
+			InitializeComponent();
+			listView.ItemsSource = Exercises;
+		}
+	}
+}
