81313d3315ab6e53a853a1e4a395f2485e31d05c
Sullivan LACHETEAU <sullivan.lacheteau@seloger.com>
2017-03-16 23:56:46 +0100

    Added timer

--- FitTrack/Pages/WorkoutPage.xaml.cs
+++ FitTrack/Pages/WorkoutPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using Xamarin.Forms;
 
 namespace FitTrack
@@ -8,6 +9,8 @@ namespace FitTrack
 	public partial class WorkoutPage : ContentPage
 	{
 		private WorkoutViewModel Workout;
+
+		public TimeSpan Timer { get; set; }
 
 		public ObservableCollection<Exercise> Exercises;
 
@@ -18,6 +21,14 @@ namespace FitTrack
 			Exercises = new ObservableCollection<Exercise>(workout.Exercises);
 			InitializeComponent();
 			listView.ItemsSource = Exercises;
+			Timer = TimeSpan.FromTicks(0);
+
+			Xamarin.Forms.Device.StartTimer(TimeSpan.FromMilliseconds(10), () => 
+			{
+				Timer = Timer.Add(TimeSpan.FromMilliseconds(10));
+				timerLabel.Text = string.Format("{0:hh\\:mm\\:ss\\.ff}", Timer);
+				return true; // false to stop timer
+			});
 		}
 	}
 }
